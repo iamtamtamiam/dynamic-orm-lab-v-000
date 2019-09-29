c72ef30fc78aaf26aae288aec91e52223d91401f
Tam Vu <vutam_a@hotmail.com>
2019-09-29 19:49:11 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -67,7 +67,7 @@ class InteractiveRecord
     sql = <<-SQL
       SELECT *
       FROM #{self.table_name}
-      WHERE #{attribute.keys} = '#{attribute[value]}'
+      WHERE #{attribute.keys} = '#{attribute[key]}'
     SQL
     DB[:conn].execute(sql)
   end 
