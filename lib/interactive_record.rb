3dbd6d271b78f33a04a17f11f966bc02aae0a458
Tam Vu <vutam_a@hotmail.com>
2019-09-29 18:37:57 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,7 +8,11 @@ class InteractiveRecord
   end
   
   def self.column_names
+    DB[:conn].results_as_hash = true
+ 
+    sql = "PRAGMA table_info('#{table_name}')"
     
+    table_info = DB[:conn].execute(sql).each do 
   end 
   
 end
